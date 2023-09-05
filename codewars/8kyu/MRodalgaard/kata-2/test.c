a1c06950c562bab3311a390ba1ea89696eb0f659
Logan Fossenier <logan.fossenier@gmail.com>
2023-09-04 21:25:49 -0600

    FOSS-2 chore: update file hierarchy

--- codewars/8kyu/MRodalgaard/kata-2/test.c
+++ codewars/8kyu/MRodalgaard/kata-2/test.c
@@ -1,15 +1,18 @@
+#include <stdlib.h>
 #include <criterion/criterion.h>
 
-char* are_you_playing_banjo(const char*);
+char *are_you_playing_banjo(const char *);
 
-void do_test(char* testCase, char* expected) {
-  char* actual = are_you_playing_banjo(testCase);
+void do_test(char *testCase, char *expected)
+{
+  char *actual = are_you_playing_banjo(testCase);
   cr_assert_str_eq(actual, expected);
-  
+
   free(actual);
 }
 
-Test(sample_tests, should_pass_all_the_tests_provided) {
+Test(sample_tests, should_pass_all_the_tests_provided)
+{
   do_test("Martin", "Martin does not play banjo");
   do_test("Rikke", "Rikke plays banjo");
   do_test("bravo", "bravo does not play banjo");
